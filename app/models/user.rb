ec40f54927d1292f4a236ed473b98f092860ca3d
EC2 Default User <ec2-user@ip-172-31-25-143.ap-northeast-1.compute.internal>
2023-02-19 20:19:43 +0000

    git

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   
   validates :name, uniqueness: true
-  validates :name,    length: { in: 2..20 }
-  validates :introduction,    length: { maximum: 50 }
+  validates :name, length: { minimum: 2 }
+  validates :name, length: { maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
   
 def get_profile_image(width, height)
   unless profile_image.attached?
